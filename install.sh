81beaf8820fbee528c0f69c61ff039cd735fa39b
Claude Pageau <pageauc@gmail.com>
2025-09-28 17:53:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ docker-compose ps
 echo "-----------------
 If calibre-web is running per above
 From a Web Browser, Access the login page Eg. http://$HOSTNAME.local:8083
-default admin username is admin with password admin123
+default admin Username is admin with Password admin123
 For more Details See Instructions above.  
 
 "
