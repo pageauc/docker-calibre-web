de898ef1d0f7b6a3c915ffad1d225d74cbb54a35
Claude Pageau <pageauc@gmail.com>
2025-09-28 15:21:37 -0400

    initial release

---
+++ download.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+CONTAINER_NAME="calibre-web"
+CURRENT_USER=$(whoami)
+
+cd ~
+mkdir -p $CONTAINER_NAME
+cd $CONTAINER_NAME
+mkdir -p library
+
+echo "Check if there are upgradable operating system packages."
+if apt list --upgradable 2>/dev/null | grep -q "Listing..."; then
+    echo "No packages need upgrading."
+else
+    echo "Packages are available for upgrade. running 'sudo apt update and sudo apt upgrade'."
+	sudo apt update
+	sudo apt upgrade -y
+fi
+
+echo "Install required OS packages if Required."
+sudo apt install cifs-utils -y   # Required for mounting remote ebook share if Required
+sudo apt install docker docker-compose -y  # install docker and docker-compose
+
+echo "Downloading calibre-web docker installation files"
+wget -O docker-compose-yml -q --show-progress https://raw.github.com/pageauc/docker-calibre-web/master/docker-compose.yml
+wget -O install.sh -q --show-progress https://raw.github.com/pageauc/docker-calibre-web/master/install.sh
+wget -O /library/metadata.db -q --show-progress https://raw.github.com/pageauc/docker-calibre-web/master/metadata.db
+wget -O Readme.md -q --show-progress https://raw.github.com/pageauc/docker-calibre-web/master/Readme.md
+chmod +x install.sh
+
+if getent group "$GROUP_NAME" >/dev/null; then
+    echo "Group '$GROUP_NAME' exists."
+else
+    echo "Adding docker group"		
+    sudo groupadd docker
+fi
+
+# Check if the user is a member of the group
+if ! id -Gn "$CURRENT_USER" | grep -q -w "docker"; then
+    echo "Add $CURRENT_USER to group"
+    sudo usermod -aG docker $CURRENT_USER
+	echo "
+	
+Logout and login to ensure you are in the docker group otherwise
+you will need to manually run
+	
+	sudo docker-compose -up	
+
+	"
+fi
+
+echo "
+                     Instructions
+
+Edit the docker-compose.yml file per commands below
+
+    cp docker-compose.yml docker-compose.yml.bak
+    nano docker-compose.yml
+   
+See comments and make changes as required.  ctr-x y  saves changes and exits.
+   
+Make sure your calibre library folder is mounted at specified share path or
+local disk folder path per setting in docker-compose.yml
+	   
+When Ready Run command below
+
+    ./install.sh   
+
+This will run calibre-web docker-compose install and provide additional Instructions
+
+for more details see https://github.com/pageauc/
+
+Exiting
+
+"
+exit 0
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo $0
+
+echo "Building/upgrading calibre-web docker-web image.  Please Wait ...."
+
+docker-compose up -d
+docker-compose ps
+
+echo "
+calibre-web login page should be available from your web browser at
+
+    http://host:port
+
+where host is the IP address or hostname  eg http://rpi-media.local:8083 or http://192.168.1.124:8083
+
+Login to calibre-web as administrator login admin  password admin123
+After login navigate to /books folder and select the metadata.db file (should be at bottom of folders listing,
+Depending on the size of your calibre library this may take a while. Be patient.
+Save setting and select books button on left.  I there is a problem check the docker-compose.yml 
+and verify library is at spcifield path share point or local folder.
+
+For details see https://github.com/janeczku/calibre-web/wiki/Configuration#basic-configuration
+
+various commads you can run
+
+    docker-compose down
+	docker-compose pull
+	docker-compose up -db
+	docker-compose ps
+
+Once logged in as admin, add/edit users and set comfiguration options using the top right admin pick
+
+for more details see https://github.com/pageauc/
+
+"
+echo $0
+echo "Bye ...."
+
